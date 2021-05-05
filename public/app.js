f30b8653986569c32e7dff82570939c7d1913161
mbenaroc <mickael_benaroch@bmc.com>
2021-05-05 10:46:25 +0300

    dummy 7

--- public/app.js
+++ public/app.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const { db, server, corss } = require('./configuration/config'), 
+const { db, server, corss } = require('../configuration/config'), 
 express        = require('express'),
 app            = express(),
 db_model       = db.db_model,
 bodyParser     = corss.bodyParser,
 cors           = corss.cors_parser
+
+//app.use(express.static('public'))
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,7 +17,7 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(cors());
 
 //route to all controller
-app.use(require('./controllers'));
+app.use(require('../controllers'));
 
 //connect to mongodb
 db_model.connect(db.connection.uri, (err) => {
